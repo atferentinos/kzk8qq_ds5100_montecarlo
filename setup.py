7343c62ad4c4b2409ee43d76926266c723f78a78
Alexandra_Ferentinos <aferentinos9@gmail.com>
2023-11-07 18:23:45 -0500

    final project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='Final Project',
+    url='https://github.com/atferentinos/kzk8qq_ds5100_montecarlo',
+    author='Alexandra Ferentinos',
+    author_email='kzk8qq@virginia.edu',
+    license='MIT',
+    packages=['Demo'],
+    install_requires=['pandas', 'numpy','matplotlib']
+)
